6d62cb02d5dfffb72a9c764e0f60060b283734a4
Michael Prokop <mika@grml.org>
2011-12-09 16:02:00 +0100

    Rework docs

--- docs/index.php
+++ docs/index.php
@@ -32,47 +32,19 @@
             <h2>FAQ &amp; zsh</h2>
 
             <p>Frequently asked questions and answers can be found in the section <a
-            href="/faq/">FAQ</a>. If you are looking for information on <a
-            href="http://zsh.sunsite.dk/">the zsh</a> take a look at the <a
+            href="/faq/">FAQ</a>. If you are looking for information on
+            the Zsh take a look at the <a
             href="/zsh/">zsh-webpage</a>.
 
-            <p>Many tips and tricks and some useful hints can be found in the <a
-            href="http://wiki.grml.org/">grml-wiki (wiki.grml.org)</a>! Share your
-            knowledge and contribute!</p>
-
-            <h2><a name="paper"></a>Paper</h2>
-
-            <p>A german paper (nowadays pretty outdated though) about grml is available online:</p>
-
-            <ul>
-              <li><a href="http://grml.org/docs/grml-paper.de.pdf">grml -
-              Linux fuer Systemadministratoren und Texttool-User (170kB, PDF)</a></li>
-            </ul>
-
-            <p>This document provides information about technologies used at grml and
-            some main stuff about grml itself.</p>
+            <p>Many tips and tricks can be found in the <a
+            href="http://wiki.grml.org/">grml-wiki (wiki.grml.org)</a>.</p>
 
             <h2>Guides</h2>
-
-            <h3>grml-live</h3>
-
-            <p><a href="/grml-live/">grml-live</a>: build framework based on FAI
-            for generating a grml and Debian based Linux Live system
-            (CD/ISO)</p>
 
             <h3>grml2usb</h3>
 
             <p><a href="/grml2usb/">grml2usb</a>: install grml ISO(s) on usb
             device for booting</p>
-
-            <h3>grml-x</h3>
-
-            <p><a href="/grml-x/">grml-x</a>: create xorg.conf file (providing
-            hardware detection) and start X window system</p>
-
-            <h3>grml2hd</h3>
-
-            <p><a href="/grml2hd/">grml2hd</a>: install grml to harddisk</p>
 
             <h3>grml-debootstrap</h3>
 
@@ -80,31 +52,20 @@
             wrapper around debootstrap for installing plain Debian via
             grml</p>
 
-            <h3>configuration handling on grml</h3>
+            <h3>grml-live</h3>
 
-            <p><a href="/config/">config</a>: save and restore configuration files</p>
+            <p><a href="/grml-live/">grml-live</a>: build framework based on FAI
+            for generating a grml and Debian based Linux Live system
+            (CD/ISO)</p>
+
+            <h3>grml-x</h3>
+
+            <p><a href="/grml-x/">grml-x</a>: create xorg.conf file (providing
+            hardware detection) and start X window system</p>
 
             <h3>grml-terminalserver</h3>
 
             <p><a href="/terminalserver/">grml-terminalserver</a>: boot grml via network</p>
-
-            <h3>Setup Xen within 20 minutes on Debian/grml</h3>
-
-            <p>How to <a href="/xen/">Setup Xen within 20 minutes on
-            Debian/grml</a></p>
-
-            <h3>emulator / qemu</h3>
-
-            <p><a href="/qemu/">qemu</a>: run grml with emulator QEMU</p>
-
-            <h3>git @ grml.org</h3>
-
-            <p>The grml-team uses <a href="http://git-scm.com/">git</a> for
-            managing software and packages. The repositories are avavilable
-            online at <a href="http://git.grml.org/">git.grml.org</a>.
-            Documentation regarding the setup and how to work with git can be
-            found at <a
-            href="/git/">grml's git webpage</a>.
 
       </div>
 
