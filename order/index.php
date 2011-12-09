64f3194d3b0524ccafce2695a47e4a480df5071c
Michael Prokop <mika@grml.org>
2011-12-09 15:18:53 +0100

    Drop outdated webshops

--- order/index.php
+++ order/index.php
@@ -27,37 +27,14 @@
 
             <p><img style="float: right" src="/img/cdrom.png" alt="*" /></p>
 
-            <h1>Order a grml CD</h1>
+            <h1>Order a Grml CD</h1>
 
-            <p>If you don't have the bandwidth to <a href="/download/">download the grml
-            iso</a> you can order a grml cd at the following webshops:</p>
+            <p>If you don't have the bandwidth to <a href="/download/">download the Grml
+            ISO</a> you can order a Grml CD at the following webshops:</p>
 
             <ul>
-              <li><a href="http://shop.tuxpost.de/index.php?manufacturers_id=66&osCsid=1f6dc8c66e7f5697d511085b2e7b61c6">tuxpost.de</a>
-              (Germany)</li>
-              <!--
-              <li><a
-              href="http://www.tuxman.de/product_info.php/products_id/303/">
-              tuxman.de</a> (Germany)</li>
-              <li><a
-              href="http://www.linuxshop.de/shop/catalog/index.php?cPath=66_77">
-              linuxshop.de</a> (Germany)</li>
-              <li><a
-              href="http://mathias-kettner.de/linux_cd_versand.html">mathias-kettner.de</a>
-              (Germany)</li>
-              <li><a
-              href="http://www.linuxcdversand.org/product_info.php?info=p85_GRML-Live-CD.html">linuxcdversand.org</a>
-              (Germany)</li>
-              -->
-              <li><a
-              href="http://www.linuxcd.org/view_distro.php?id_distro=298">linuxcd.org</a>
-              (Worldwide)</li>
-              <li><a
-              href="http://www.osdisc.com/cgi-bin/view.cgi/products/livecd/grml">osdisc.com</a>
-              (Worldwide)</li>
-              <!--
-              <li><a href="http://www.frozentech.com/operating-system/linux/grml">frozentech.com</a> (Worldwide)</li>
-              -->
+              <li><a href="http://shop.tuxpost.de/index.php?manufacturers_id=66&osCsid=1f6dc8c66e7f5697d511085b2e7b61c6">tuxpost.de</a> (Germany)</li>
+              <li><a href="http://www.linuxcd.org/view_distro.php?id_distro=298">linuxcd.org</a> (Worldwide)</li>
             </ul>
 
       </div>
