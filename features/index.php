99fdad51a50bd5c3780745d0dc3fa50fdfa6c559
Michael Prokop <mika@grml.org>
2011-12-09 15:51:28 +0100

    Rewordings in feature/

--- features/index.php
+++ features/index.php
@@ -27,48 +27,48 @@
 
           <p><a href="http://www.catb.org/hacker-emblem/"><img style="float: right; border: 0" src="/img/glider.png" alt="*" /></a></p>
 
-          <h1>Features and main goals of grml</h1>
+          <h1>Features and main goals of Grml</h1>
 
           <ul>
               <li>distribution for sysadmins and users of text tools</li>
               <li>Debian based distribution [use existing infrastructure]</li>
               <li>usability by the visually impaired right out of the box</li>
-              <li>geeky community</li>
             </ul>
 
-          <p><a href="/zsh/"><img style="border: 0" src="/img/zsh_lover.png" alt="*"
-          /></a> Grml - Linux for zsh lovers</p>
+          <p><a href="/zsh/"><img style="border: 0"
+          src="/img/zsh_lover.png" alt="*" /></a> Grml - Linux for zsh
+          lovers</p>
 
-          <p>Grml provides more than 2500 software packages. Excluding library
-          stuff, more than 1700 packages remain. We don't ship KDE and
-          OpenOffice, but more than 800 packages which for example Knoppix does
-          not provide.  You'll get <strong>sysadmin's favourite tools</strong>,
-          security- and network-related software, data recovery- and
-          forensic-tools, many editors, shells, and of course many
-          texttools. Choose between different flavours (grml, grml-medium and
-          grml-small) and a x86 and a amd64 version depending on your needs.</p>
+          <p>Grml provides an excellent selection of software packages
+          for installation, deployment and system rescue, You'll
+          get <strong>sysadmin's favourite tools</strong>, security-
+          and network-related software, data recovery- and
+          forensic-tools. Choose between a x86 and an amd64 version
+          depending on your needs.</p>
 
-          <p>Grml provides several scripts and utils which <strong>make life
-            easier</strong>. The default interactive shell is <a
-            href="/zsh/">Zsh</a> and is configured with a very powerful setup,
-          including a nifty completion mechanism. grml-x is a wrapper for
-          configuring and using the X window system. <a
-            href="/grml2usb/">grml2usb</a> is a tool for installing grml ISO(s)
-          on a usb device for booting. grml-crypt is a program that provides an
-          easy wrapper arround cryptsetup, mkfs, losetup and mount. <a
-            href="/grml-live/">grml-live</a> is a build framework based on <a
-            href="http://fai-project.org/">FAI</a> (Fully
-          Automatic Installation) for generating a grml and Debian based Linux
-          Live system (CD/ISO). grml-tips provides useful tips and tricks for
-          daily life on the command line. grml-vpn is a program to establish
-          encrypted communication channels in a network, and so on...</p>
+          <p>Grml provides several scripts and utils
+          which <strong>make life easier</strong>. The default
+          interactive shell is <a href="/zsh/">Zsh</a> and is
+          configured with a very powerful setup, including a nifty
+          completion mechanism. grml-x is a wrapper for configuring
+          and using the X window
+          system. <a href="/grml2usb/">grml2usb</a> is a tool for
+          installing Grml ISO(s) on a usb device for
+          booting. grml-crypt is a program that provides an easy
+          wrapper arround cryptsetup, mkfs, losetup and
+          mount. <a href="/grml-live/">grml-live</a> is a build
+          framework based on <a href="http://fai-project.org/">FAI</a>
+          (Fully Automatic Installation) for generating a Grml and
+          Debian based Linux Live system (CD/ISO). grml-tips provides
+          useful tips and tricks for daily life on the command
+          line. </p>
 
-          <p>Visit the <a href="/faq/">FAQ</a> for answers to some Frequently Asked
-          Questions.</p>
+          <p>Visit the <a href="/faq/">FAQ</a> for answers to some
+          Frequently Asked Questions.</p>
 
-          <p>You can browse the list of software (packages) provided by
-          grml - check out the <a href="/files/#debian">Debian section
-          at grml.org/files/</a>.</p>
+          <p>You can browse the list of software (packages) provided
+          by Grml - check out the <a href="/files/#debian">Debian
+          section</a>.</p>
 
         </div>
 
