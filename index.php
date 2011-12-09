9f09397c57d4974587087c6851c234a4ed576fdf
Michael Prokop <mika@grml.org>
2011-12-09 15:39:37 +0100

    Simplify startpage

--- index.php
+++ index.php
@@ -35,14 +35,10 @@
             <p>Grml is a bootable live system (Live-CD) based on <a
               href="http://www.debian.org/">Debian</a>. Grml includes a
             collection of GNU/Linux software especially for system
-            administrators and users of texttools. Grml provides automatic
-            hardware detection. You can use Grml (for example) as a rescue
-            system, for analyzing systems/networks or as a working environment.
-            It is not necessary to install anything to a harddisk; you don't
-            even need a harddisk to run it. Due to on-the-fly decompression Grml
-            includes about 2.1 GB of software and documentation on the CD. You
-            don't have to pay anything to use Grml - it is available for free!
-            <a href="/features/">Read more...</a></p>
+            administrators. It is not necessary to install anything to a
+            harddisk. It specializes on administrative tasks like installation,
+            deployment and system rescue. <a href="/features/">Read
+            more...</a></p>
 
             <br />
 
@@ -56,13 +52,7 @@
 
             <h2>News</h2>
             <div class="news">
-            <p>29 May 2011: New <a href="/changelogs/README-grml-2011.05/">stable release version 2011.05</a> is now available <a href="/download/">for download</a>.</p>
-            <p>01 Apr 2011: Grml joins <a href="http://blog.grml.org/archives/352-The-Canterbury-Project.html">The Canterbury Project</a>.</p>
-            <p>20 Mar 2011: Grml development blog <a href="http://blog.grml.org/archives/350-A-new-home.html">moved to new home</a> - now available at <a href="http://blog.grml.org/">blog.grml.org</a>.</p>
-            <p>11 Mar 2011: <a href="http://grml.org/survey2011-results/">Results of the Grml user survey</a> available.</p>
-            <p>09 Mar 2011: German magazine <a href="http://www.heise.de/ct/meldung/c-t-extra-Netzwerke-ab-sofort-bestellbar-1200214.html">c't extra Netzwerke 01/2011</a> features a special edition of Grml 2010.12.</p>
-            <p>03 Mar 2011: German <a href="http://www.linux-magazin.de/Heft-Abo/Ausgaben/2011/03">Linux Magazine 03/11</a> features Grml 2010.12 on DVD (DELUG version).</p>
-            <p>31 Dec 2010: New <a href="/changelogs/README-grml-2010.12/">stable release 2010.12</a> is <a href="/download/">available</a>.</p>
+            <p>29 May 2011: New <a href="/changelogs/README-grml-2011.05/">stable release version 2011.05</a> is available <a href="/download/">for download</a>.</p>
             </div>
 
       </div>
