162d5e13de80aeb6ea736e4fa0aaa20887fcff7f
Michael Prokop <mika@grml.org>
2020-12-04 11:10:25 +0100

    scripts/grml-config.sh: include .tmux.conf
    Related to https://github.com/grml/grml-etc-core/pull/115

--- scripts/grml-config.sh
+++ scripts/grml-config.sh
@@ -2,6 +2,7 @@
 # IMPORTANT: please note that you might override existing
 # configuration files in the current working directory! =>
 wget -O .screenrc     http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic
+wget -O .tmux.conf    http://git.grml.org/f/grml-etc-core/etc/tmux.conf
 wget -O .vimrc        http://git.grml.org/f/grml-etc-core/etc/vim/vimrc
 wget -O .zshrc        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 # optional:
