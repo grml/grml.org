ca19d03de10eb30e24cf417729e8e2f015d64ab8
Michael Prokop <mika@grml.org>
2020-12-04 11:10:50 +0100

    scripts/grml-config.sh: use https instead of http

--- scripts/grml-config.sh
+++ scripts/grml-config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # IMPORTANT: please note that you might override existing
 # configuration files in the current working directory! =>
-wget -O .screenrc     http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic
-wget -O .tmux.conf    http://git.grml.org/f/grml-etc-core/etc/tmux.conf
-wget -O .vimrc        http://git.grml.org/f/grml-etc-core/etc/vim/vimrc
-wget -O .zshrc        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+wget -O .screenrc     https://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic
+wget -O .tmux.conf    https://git.grml.org/f/grml-etc-core/etc/tmux.conf
+wget -O .vimrc        https://git.grml.org/f/grml-etc-core/etc/vim/vimrc
+wget -O .zshrc        https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 # optional:
-# wget -O .zshrc.local        http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
+# wget -O .zshrc.local        https://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
